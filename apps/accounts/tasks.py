e1ee7e36de206368b7c86fd5a6dcae639a05abc5
prime2014 <omondiprime@gmail.com>
2022-04-13 00:30:41 -0700

    added django apps

---
+++ apps/accounts/tasks.py
@@ -0,0 +1,27 @@
+from config.celery_app import app as celery_app
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.conf import settings
+from django.core.mail import send_mail
+import environ
+
+
+@celery_app.task()
+def send_activation_link(token, user):
+    """A celery task to send an account activation link to the user email"""
+    context = {
+        'firstname': user.get("first_name"),
+        "lastname": user.get("last_name"),
+        "token": token,
+        "origin": settings.CORS_ALLOWED_ORIGINS[0]
+    }
+
+    msg_html = render_to_string("accounts/account_activation.html", context=context)
+    email = user.get("email")
+    send_mail(
+        "Account Activation",
+        msg_html,
+        from_email= settings.DEFAULT_FROM_EMAIL,
+        recipient_list=[email],
+        html_message=msg_html
+    )
